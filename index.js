14629b7d036a30e2a951e0aa6b355d073ab91d48
samekh_resh <C0d1ng4Lyf3>
2022-04-27 16:53:34 -0400

    changed dev added process

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 // const { application } = require("express")
 const express = require("express")
 
+console.log(process.env.PORT)
+
 const server = express() // this server cannot hear a single thing
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+// const PORT = 3000
 
 server.listen(PORT, () =>{
     console.log("server...she listening...")
@@ -16,6 +19,5 @@ server.get("/samekh", (req, res) =>{
     res.send("<h1> hi i'm samekh! </h1>"
     )
 })
-
 
 //two homeworks that we
